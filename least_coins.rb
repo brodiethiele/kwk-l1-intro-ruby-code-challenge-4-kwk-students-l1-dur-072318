ce1ee4cf87320a3ea9eaf3bf890fda977527de9d
brodiethiele <brodie.thiele@gmail.com>
2018-07-27 13:23:33 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -9,25 +9,25 @@ def least_coins(x)
  elsif x%25!=0
     leastCoins[:quarter] = x/25
     remainder=x%25
-end
+  end
 
 if remainder%10==0
   puts leastCoins[:dime] = remainder/10
   puts remainder/10 
   elsif x/10 >=1 && x%10!=0
   # continue to next method
-
+  end 
 if remainder%5==0
   puts remainder/5
   elsif x%5 ==!0
   # continue to next method
-end
+  end
 if remainder%1==0
   puts leastCoins[:penny]
   
 
 #Code your answer here!
 
-end
+  end
 end
 least_coins(35)
