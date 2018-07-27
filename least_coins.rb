73d210bd11e89ae5156bb28d4ba5291c95141625
brodiethiele <brodie.thiele@gmail.com>
2018-07-27 13:21:16 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,5 +1,5 @@
 #write out your code here
-{:quarter=>25, :dime => 10, :nickel => 5, :penny => 1}
+coins={:quarter=>25, :dime => 10, :nickel => 5, :penny => 1}
 
 leastCoins = {}
 def least_coins(x)
@@ -13,16 +13,17 @@ end
 
 if remainder%10==0
   puts leastCoins[:dime] = remainder/10
-  puts x/10 
+  puts remainder/10 
   elsif x/10 >=1 && x%10!=0
   # continue to next method
 
-if x%5==0
-  puts x/5
+if remainder%5==0
+  puts remainder/5
   elsif x%5 ==!0
   # continue to next method
 end
-  
+if remainder%1==0
+  puts leastCoins[:penny]
   
 
 #Code your answer here!
